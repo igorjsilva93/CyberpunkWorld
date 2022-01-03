08b45488b899d16ca8505186c87ba156fd706258
kirakun123 <50359719+kirakun123@users.noreply.github.com>
2022-01-03 10:05:26 +0000

    V1
    Alteração do navigator e criação do books.php

--- books.php
+++ books.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <div class="cabecalho">
+        <title>Cyberpunk World</title>
+        <link rel="stylesheet" href="style.css">
+    </div>
+</head>
+<body>
+<div class="logo">
+    <center><img src="logo.png"/></center>
+  </div>
+  <div class="navegacao">
+  <nav>
+    <a href="index.html">Home</a> |
+    <a href="sobre.html">About</a> |
+    <a href="historia.html">Storyline (CP2077)</a> |
+    <a href="rpg.html">RPG</a> |
+    <a href="movies.html">Movies</a>
+    <a href="books.php">Books</a>
+</nav>
+
 A Song Called Youth Trilogy by John Shirley (1985)
